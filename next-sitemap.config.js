1c037496857fc9c08bebd03b92dee5f297aa0fdb
Rakibul islam Bappi <bappi616@gmail.com>
2025-10-25 21:20:25 +0600

    Configure next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://yumzy-web.vercel.app', // Replace with your actual Vercel URL
+  generateRobotsTxt: true, // Optional: Generates a robots.txt file too
+  // Add other options here if needed, like excluding pages
+};
