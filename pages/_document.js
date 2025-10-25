4ab75ccd6cb04a56c76d8e9bef2c874bc4aab795
Rakibul islam Bappi <bappi616@gmail.com>
2025-10-25 21:02:44 +0600

    meta to to document file for SEO

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Head from 'next/head';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+     <Head>
+        {/* --- PLACE YOUR TAGS HERE --- */}
+        <meta name="google-site-verification" content="o_ZP7Gh228bnvaAsyMyjC4LDXViRYDT5ic31HO4qgOM" />
+        <title>Yumzy - Daffodil Smart City’s Food & Grocery Solution</title>
+        <meta name="description" content="Yumzy lets Daffodil Smart City residents order food and groceries quickly and conveniently." />
+        {/* --- END OF YOUR TAGS --- */}
+
+       
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
