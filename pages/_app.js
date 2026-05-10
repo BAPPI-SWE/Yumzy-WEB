a9109b01ebe76a7d96c7f2c3c4f2591639e35349
Rakibul islam Bappi <bappi616@gmail.com>
2026-05-10 15:54:01 +0600

    title

--- pages/_app.js
+++ pages/_app.js
@@ -51,8 +51,8 @@ function MyApp({ Component, pageProps }) {
         <Head>
         <meta name="google-site-verification" content="o_ZP7Gh228bnvaAsyMyjC4LDXViRYDT5ic31HO4qgOM" />
         {/* You can also add your site title here */}
-        <title>Yumzy - Daffodil Smart City’s Food & Grocery Solution</title>
-        <meta name="description" content="Yumzy lets Daffodil Smart City residents order food and groceries quickly and conveniently." />
+        <title>Foodish - Daffodil Smart City’s Food & Grocery Solution</title>
+        <meta name="description" content="Foodish lets Daffodil Smart City residents order food and groceries quickly and conveniently." />
       </Head>
         <CartProvider>
           {useMainLayout ? (
